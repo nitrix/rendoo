741d9186249345aa8220b54ae0f7d5e83c765b14
Alex Belanger <i.caught.air@gmail.com>
2018-09-06 21:43:40 -0400

    Calculating the brightness of the rasterized pixel by interpolating the vertices normals (barycentric again).

--- triangle.go
+++ triangle.go
@@ -39,18 +39,39 @@ func drawFilledTriangle(img *image.RGBA, v1, v2, v3 Point, c color.Color) {
 	}
 }
 
-func drawFilledTriangleZBuffer(img *image.RGBA, v1, v2, v3 Point, zBuffer []float64, face Face, c color.Color) {
+func drawFilledTriangleZBuffer(img *image.RGBA, v1, v2, v3 Point, zBuffer []float64, face Face) {
 	width := img.Bounds().Dx()
+
+	lightSource := Vertex{0, 0, 1}
 
 	min, max := boundingBox(v1, v2, v3)
 	for x := min.X; x <= max.X; x++ {
 		for y := min.Y; y <= max.Y; y++ {
 			if pointInTriangle(Point{x, y}, v1, v2, v3) {
 				w1, w2, w3 := barycentric(Point{x,y}, v1, v2, v3)
+
+				// Interpolate Z based on barycentric weights
 				z := w1 * face.Vertices[0].Z + w2 * face.Vertices[1].Z + w3 * face.Vertices[2].Z
 
+				// Interpolate normal based on barycentric weights
+				normal := Vertex {
+					X: w1 * face.Normals[0].X + w2 * face.Normals[1].X + w3 * face.Normals[2].X,
+					Y: w1 * face.Normals[0].Y + w2 * face.Normals[1].Y + w3 * face.Normals[2].Y,
+					Z: w1 * face.Normals[0].Z + w2 * face.Normals[1].Z + w3 * face.Normals[2].Z,
+				}
+				normal.normalize(1.0)
+
+				// Calculate light intensity
+				intensity := normal.X * lightSource.X + normal.Y * lightSource.Y + normal.Z * lightSource.Z
+
+				if intensity < 0 {
+					continue
+				}
+
+				// Drawing according to Z-buffer
 				if zBuffer[width*y+x] < z {
 					zBuffer[width*y+x] = z
+					c := color.RGBA{R: uint8(intensity * 255), G: uint8(intensity * 255), B: uint8(intensity * 255), A: 255}
 					img.Set(x, y, c)
 				}
 			}
